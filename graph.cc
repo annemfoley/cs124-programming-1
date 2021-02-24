e7c4234def18e9b67594b9589039068d2b165804
annemfoley <31217196+annemfoley@users.noreply.github.com>
2021-02-24 09:38:52 -0500

    finished polishing random graph generation

---
+++ graph.cc
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <cstdio>
+#include <stdlib.h>
+#include <cmath>
+#include <string>
+#include <time.h> 
+
+
+// representation of our generated graph
+struct Graph{
+    unsigned long n; // # of vertices
+    int d; // dimension, -1 = custom
+    double* V; // array of vertices, use for >=1 dimension
+    double* E; // matrix of edge weights, use for 0 dimension
+
+    void init_graph();
+    Graph(unsigned long num_v, int dim){
+        this->n = num_v;
+        this->d = dim;
+        if(this->d<1){
+            this->E = (double*) malloc(sizeof(*(this->E)) * this->n * this->n);
+        }
+        else{
+            this->V = (double*) malloc(sizeof(*(this->V)) * this->n * this->d);
+        }
+        this->init_graph();
+    };
+
+    double calculate_edge(unsigned long v1, unsigned long v2);
+
+    double kruskal();
+    double prim();
+};
+
+
+
+
+// create our random graph
+void Graph::init_graph(){
+
+    // handle 0 dimension
+    if(d < 1){
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<i; j++){
+                double edge_weight =  (double) rand() / (double) (RAND_MAX-1);
+                E[i*n+j] = edge_weight;
+                E[j*n+i] = edge_weight;
+            }
+            E[i*n+i] = 0;
+        }
+    }
+
+    // handle >= 1 dimension
+    else{
+        for(int i=0; i<this->n; i++){
+            for(int k=0; k<this->d; k++){
+                V[i*d+k] = (double) rand() / (double) (RAND_MAX-1);
+            }
+        }
+    }
+}
+
+// return the edge weight between v1 and v2
+double Graph::calculate_edge(unsigned long v1, unsigned long v2){
+    if(d < 1){
+        return E[v1*n + v2];
+    }
+    else{
+        double edge_weight_sq = 0;
+        for(int k = 0; k<d; k++){
+            edge_weight_sq += pow(V[v1*d+k] - V[v2*d+k],2);
+        }
+        return sqrt(edge_weight_sq);
+    }
+}
+
+
+
+double Graph::kruskal(){
+    return 0;
+}
+
+
+double Graph::prim(){
+    return 0;
+}
+
---
+++ randmst.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cstdio>
+#include <stdlib.h>
+#include <cmath>
+#include <string>
+#include <time.h> 
+
+#include "./graph.cc"
+
+void display(Graph g){
+    printf("PRINTING EDGE WEIGHTS:\n");
+
+    if(g.d >=1){
+        for(int i = 0; i<g.n; i++){
+            printf("vertex %d:\t", i);
+            for(int k = 0; k<g.d; k++){
+                printf("%f\t", g.V[i*g.d+k]);
+            }
+            printf("\n");
+        }
+    }
+
+    printf("-------------------\n");
+    for(int i=0; i<g.n; i++){
+        printf("[\t");
+        for(int j=0; j<g.n; j++){
+            printf("%f\t", g.calculate_edge(i,j));
+        }
+        printf("]\n");
+    }
+    
+}
+
+
+int main(int argc, char** argv) {
+    int seed = std::stoi(argv[1]);
+    srand(time(NULL)); // generate seed
+    unsigned long n = std::stoi(argv[2]);
+    int dim = std::stoi(argv[3]);
+
+    Graph g = Graph(n, dim);
+    display(g);
+
+    if(dim < 1){
+        free(g.E);
+    }
+    else{
+        free(g.V);
+    }
+}
